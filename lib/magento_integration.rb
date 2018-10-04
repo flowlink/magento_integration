43029e27ef6b03f26c5391b3444ff68831503b13
bcgianni <bcgianni@gmail.com>
2018-10-04 20:49:47 -0300

    Include both rest and soap files

--- lib/magento_integration.rb
+++ lib/magento_integration.rb
@@ -1,6 +1,7 @@
 $:.unshift File.dirname(__FILE__)
 
-require 'magento_integration/services/base'
+require 'magento_integration/services/soap'
+require 'magento_integration/services/rest'
 require 'magento_integration/base'
 require 'magento_integration/order'
 require 'magento_integration/product'
