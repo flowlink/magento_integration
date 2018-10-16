7903837c9db10576db71b4c46b9353369d651d5b
bcgianni <bcgianni@gmail.com>
2018-10-16 19:59:06 -0300

    Build spec of expected attributes

--- spec/magento_endpoint_spec.rb
+++ spec/magento_endpoint_spec.rb
@@ -27,11 +27,44 @@ describe MagentoEndpoint do
     end
 
     context 'with long since param' do
-      it 'retrieves the last magento orders correctly' do
-        VCR.use_cassette("post get_orders", record: :new_episodes) do
+      it 'retrieves the last magento orders without errors' do
+        VCR.use_cassette('post get_orders', record: :new_episodes) do
           post '/get_orders', post_payload
 
           expect(last_response).to be_ok
+        end
+      end
+
+      it 'retrieves all attributes it has to' do
+        VCR.use_cassette('post get_orders', record: :new_episodes) do
+          post '/get_orders', post_payload
+
+          first_order = JSON.parse(last_response.body)['orders'][0]
+          first_line_item = first_order['line_items'][0]
+
+          expect(first_order).to include('created_at')
+          expect(first_order).to include('order_id')
+          expect(first_order).to include('status')
+          expect(first_order).to include('customer_firstname')
+          expect(first_order).to include('customer_lastname')
+          expect(first_order).to include('order_currency_code')
+          expect(first_order).to include('shipping_method')
+          expect(first_order).to include('store_to_order_rate')
+          expect(first_order).to include('comments')
+          expect(first_order).to include('billing_address')
+          expect(first_order).to include('shipping_address')
+          expect(first_order).to include('updated_at')
+          expect(first_order).to include('magento_order_id')
+          expect(first_order).to include('purchased_from')
+          expect(first_order).to include('shipping_price')
+          expect(first_order).to include('line_items')
+          # expect(first_order).to include('sales_representative')
+
+          expect(first_line_item).to include('name')
+          expect(first_line_item).to include('qty_ordered')
+          expect(first_line_item).to include('price')
+          # expect(line_items).to include('discount_amount')
+          # expect(line_items).to include('zoho_description')
         end
       end
     end
